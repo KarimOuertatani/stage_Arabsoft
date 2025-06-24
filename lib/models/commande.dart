99a8a48f2ec12311943a45ba86afdedb58b5f8d7
karimouertatani <mohamedkarim.ouertatani@esprit.tn>
2025-06-24 22:43:40 +0100

    ajout total

--- lib/models/commande.dart
+++ lib/models/commande.dart
@@ -2,18 +2,26 @@ class Commande {
   final int? id;
   final int clientId;
   final String statut;
+  final double? total;
 
-  Commande({this.id, required this.clientId, required this.statut});
+  Commande({
+    this.id,
+    required this.clientId,
+    required this.statut,
+    this.total,
+  });
 
   factory Commande.fromJson(Map<String, dynamic> json) => Commande(
         id: json['id'],
         clientId: json['clientId'],
         statut: json['statut'],
+        total: json['total'] != null ? json['total'].toDouble() : null,
       );
 
   Map<String, dynamic> toJson() => {
         if (id != null) 'id': id,
         'clientId': clientId,
         'statut': statut,
+        if (total != null) 'total': total,
       };
 }
